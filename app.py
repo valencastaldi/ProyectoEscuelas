45cc807e6e0de9430fb210fa83f322ea985bef62
valencastaldi <tinocastaldi04@gmail.com>
2025-07-10 10:11:32 -0300

    10/07

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 app.secret_key = 'clave_secreta_para_sesiones'
 
 # Registrar Blueprints
-app.register_blueprint(comunicados_bp)
 app.register_blueprint(auth_bp)
-app.register_blueprint(admin_bp)
-app.register_blueprint(profesor_bp)
-app.register_blueprint(alumno_bp)
+app.register_blueprint(admin_bp, url_prefix='/admin')
+app.register_blueprint(profesor_bp, url_prefix='/profesor')
+app.register_blueprint(alumno_bp, url_prefix='/alumno')
+app.register_blueprint(comunicados_bp, url_prefix='/comunicados')
 
 if __name__ == '__main__':
     app.run(debug=True)
