a5cc06ae57e1ae0de871528e2af15276a9c43395
valencastaldi <tinocastaldi04@gmail.com>
2025-08-19 12:58:01 -0300

    03/07

--- backend/conexion.py
+++ backend/conexion.py
@@ -1,10 +1,15 @@
+import os
+import sys
+from contextlib import contextmanager
+
 import psycopg2
 import psycopg2.extras
-import os
 from psycopg2 import pool
-from contextlib import contextmanager
-import sys
-from backend.logger_base import log  # Si usás logger, mantenelo
+from backend.logger_base import log
+from dotenv import load_dotenv
+
+# Cargar variables desde .env
+load_dotenv()
 
 class Conexion:
     _pool = None
@@ -19,25 +24,30 @@ class Conexion:
                 db_name = os.environ.get('DB_NAME', 'postgres')
                 db_user = os.environ.get('DB_USER', 'postgres')
                 db_password = os.environ.get('DB_PASSWORD', '')
-                db_port = os.environ.get('DB_PORT', '5432')
-                db_sslmode = os.environ.get('DB_SSLMODE', 'require')  # default "require" para Render
+                db_port = int(os.environ.get('DB_PORT', '5432'))
 
-                cls._pool = pool.SimpleConnectionPool(
-                    cls._MIN_CON,
-                    cls._MAX_CON,
+                conn_kwargs = dict(
                     host=db_host,
                     database=db_name,
                     user=db_user,
-                    password=db_password,
                     port=db_port,
-                    sslmode=db_sslmode,
                     cursor_factory=psycopg2.extras.DictCursor
+                )
+                if db_password:
+                    conn_kwargs['password'] = db_password
+                    log.debug('Usando autenticación con password.')
+                else:
+                    log.debug('Intentando conexión SIN password (requiere trust en pg_hba.conf).')
+
+                log.debug(f'Creando pool: host={db_host} port={db_port} db={db_name} user={db_user}')
+                cls._pool = pool.SimpleConnectionPool(
+                    cls._MIN_CON, cls._MAX_CON, **conn_kwargs
                 )
                 log.debug(f'Creación del pool exitosa {cls._pool}')
                 return cls._pool
             except Exception as e:
-                log.error("Ocurrió un error al obtener el pool: %s", repr(e))
-                sys.exit()
+                log.error('Ocurrió un error al obtener el pool: %s', repr(e))
+                sys.exit(1)
         else:
             return cls._pool
 
